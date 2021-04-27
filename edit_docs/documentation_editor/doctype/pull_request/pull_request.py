a2486f7bb9a549eec2e469189ecbf697cb405e52
Mohammad Hasnain Mohsin Rajan <hasnain2808@gmail.com>
2021-04-27 19:19:27 +0530

    fix: popen

--- edit_docs/documentation_editor/doctype/pull_request/pull_request.py
+++ edit_docs/documentation_editor/doctype/pull_request/pull_request.py
@@ -174,7 +174,7 @@ def popen(command, *args, **kwargs):
 
 	if proc.returncode and raise_err:
 		frappe.throw(
-				errs, title=_(command[:100] if not command else "subprocess error" )
+				errs, title=_(command[0:100])
 			)
 
 	return (outs, errs)
